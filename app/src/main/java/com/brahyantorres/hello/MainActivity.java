92e63f06662ddf88e141568972e097810f461607
brahyan.torres <brahyan.torres@pascualbravo.edu.co>
2023-08-28 20:42:28 -0500

    Primer app2

--- app/src/main/java/com/brahyantorres/hello/MainActivity.java
+++ app/src/main/java/com/brahyantorres/hello/MainActivity.java
@@ -2,12 +2,25 @@ package com.brahyantorres.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView Mensaje_Visible;
+    EditText Nombre_Usuario;
+    int username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Mensaje_Visible = findViewById(R.id.Mensaje_Visible);
+        Nombre_Usuario = findViewById(R.id.Nombre_Usuario);
+
+        Mensaje_Visible.setText("Hola señor Usuario");
+
+        username = Integer.parseInt(Nombre_Usuario.getText().toString());
+
     }
 }
